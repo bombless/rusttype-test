42dc00db50d21b0aefd5c303a50bb519108ae43e
York Xiang <bombless@126.com>
2025-11-09 16:19:25 +0800

    Done

--- src/main.rs
+++ src/main.rs
@@ -17,40 +17,31 @@ fn main() {
 
     let v_metrics = font.v_metrics(scale);
 
-    for g in font.layout("a\nb\r\t", scale, point(20.0, 20.0 + v_metrics.ascent)) {
-        println!("{g:?}")
-    }
-
-    // layout the glyphs in a line with 20 pixels padding
-    let glyphs: Vec<_> = font
-        .layout(text, scale, point(20.0, 20.0 + v_metrics.ascent))
-        .collect();
-
     let mut data = vec![0u8; 800 * 800 * 3];
-    let mut origin = point(0, 0);
-    // Loop through the glyphs in the text, positing each one on a line
-    let mut already_skipped = false;
-    let mut first = true;
-    for glyph in glyphs {
+    let mut cursor = point(0.0, v_metrics.ascent);
+    let mut last_glyph = None;
+    for c in text.chars() {
+        let glyph = font.glyph(c);
+        let id = glyph.id();
+        let scaled = glyph.scaled(scale);
+        let w = scaled.h_metrics().advance_width;
+        if let Some(last) = last_glyph {
+            cursor.x += font.pair_kerning(scale, last, id);
+        }
+        let glyph = scaled.positioned(cursor);
+        cursor.x += w;
         if let Some(bounding_box) = glyph.pixel_bounding_box() {
-            let skip = if already_skipped {
-                false
-            } else {
-                already_skipped = glyph.id().0 == 0;
-                already_skipped
-            };
-            if first {
-                println!("skip {skip}");
-                first = false;
+            if c == '\n' || bounding_box.max.x + cursor.x.ceil() as i32 >= 800 {
+                cursor.x = 0.0;
+                cursor.y += 32.0;
             }
-            if bounding_box.max.x + origin.x >= 800 {
-                origin.x -= 800;
-                origin.y += 32;
+            if c == '\n' {
+                continue;
             }
             // Draw the glyph into the image per-pixel by using the draw closure
             glyph.draw(|x, y, v| {
-                let x = x as i32 + bounding_box.min.x + origin.x;
-                let y = y as i32 + bounding_box.min.y + origin.y;
+                let x = x as i32 + bounding_box.min.x;
+                let y = y as i32 + bounding_box.min.y;
                 if x >= 0 && y >= 0 && x < 800 && y < 800 {
                     let x = x as usize;
                     let y = y as usize;
@@ -62,6 +53,7 @@ fn main() {
                 }
             });
         }
+        last_glyph = Some(id);
     }
 
     put_image(800, 800, &data);

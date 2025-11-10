5cb0dfcbced567a1ecf6cadd64ac63f9a5ad10a0
York Xiang <bombless@126.com>
2025-11-10 16:59:04 +0800

    调整测试渲染文本

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
-    // The text to render
-    let text = include_str!("main.rs");
-    let data = lib::make(800, text);
+    let mut data = Vec::new();
+    for _ in 0..10 {
+        data.push(42);
+        data.push(0);
+        data.push(0)
+    }
+    let text = format!("{data:?}");
+    let data = lib::make(800, &text);
     lib::put_image("main", 800, data.len() as u32 / 800 / 3, &data);
 }
